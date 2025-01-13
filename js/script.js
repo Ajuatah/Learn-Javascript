3f21cbd24fe0ec8280060d24caa8b7601fbf69b5
techritoma <devtechritoma.com>
2025-01-13 13:01:51 +0100

    Print hello wolrd

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function hello() {
+    console.log('Hello!');
+    console.log('Welcome to JavaScript');
+    }
+hello();
+hello();
+
+for(let i =0; i<5; i++){
+    console.log("I am from loop");
+    hello();
+}
